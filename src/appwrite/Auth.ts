8ee9c76ce222984169859dc808419044f868e73a
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-06-23 01:02:31 +0530

    fix: types

--- src/appwrite/Auth.ts
+++ src/appwrite/Auth.ts
@@ -1,11 +1,12 @@
 import { account } from "./appwrite";
 import { ID } from "./appwrite";
 
+// User Login
 export const SignIn = async (email: string, password: string) => {
 	const login = await account.createEmailPasswordSession(email, password);
 	return login;
 };
-
+// User Registration
 export const Register = async (email: string, password: string, name: string | undefined) => {
 	const register = await account.create(ID.unique(), email, password, name);
 	return register;
